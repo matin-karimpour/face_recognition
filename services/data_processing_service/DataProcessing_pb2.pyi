67421da9134254e1e6c8855ecb5bd777542950d0
Matin karimpour <matin_karimpour@pm.me>
2024-04-22 21:46:25 +0330

    add image and track_id in reply for data processing service

--- services/data_processing_service/DataProcessing_pb2.pyi
+++ services/data_processing_service/DataProcessing_pb2.pyi
@@ -5,17 +5,25 @@ from typing import ClassVar as _ClassVar, Optional as _Optional
 DESCRIPTOR: _descriptor.FileDescriptor
 
 class Replyresult(_message.Message):
-    __slots__ = ("msg",)
+    __slots__ = ("image", "track_id", "msg")
+    IMAGE_FIELD_NUMBER: _ClassVar[int]
+    TRACK_ID_FIELD_NUMBER: _ClassVar[int]
     MSG_FIELD_NUMBER: _ClassVar[int]
+    image: bytes
+    track_id: int
     msg: int
-    def __init__(self, msg: _Optional[int] = ...) -> None: ...
+    def __init__(self, image: _Optional[bytes] = ..., track_id: _Optional[int] = ..., msg: _Optional[int] = ...) -> None: ...
 
 class RequestFace(_message.Message):
-    __slots__ = ("images", "action", "name")
+    __slots__ = ("images", "action", "name", "track_ids", "frame_index")
     IMAGES_FIELD_NUMBER: _ClassVar[int]
     ACTION_FIELD_NUMBER: _ClassVar[int]
     NAME_FIELD_NUMBER: _ClassVar[int]
+    TRACK_IDS_FIELD_NUMBER: _ClassVar[int]
+    FRAME_INDEX_FIELD_NUMBER: _ClassVar[int]
     images: bytes
     action: str
     name: str
-    def __init__(self, images: _Optional[bytes] = ..., action: _Optional[str] = ..., name: _Optional[str] = ...) -> None: ...
+    track_ids: bytes
+    frame_index: int
+    def __init__(self, images: _Optional[bytes] = ..., action: _Optional[str] = ..., name: _Optional[str] = ..., track_ids: _Optional[bytes] = ..., frame_index: _Optional[int] = ...) -> None: ...
